99c8a4cecca4bf572c4dc7bc9ad43a9be3368e0a
Niccolò Mazzoleni <niccolomazzoleni@gmail.com>
2026-09-23 11:34:29 +0200

    deploy: confronto assessment

--- assets/arrow-right-HTlq4-1x.js
+++ assets/arrow-right-HTlq4-1x.js
@@ -1,4 +1,4 @@
-import{r as n}from"./index-CgfjTrW5.js";/**
+import{r as n}from"./index-DnsotuRw.js";/**
  * @license lucide-react v0.469.0 - ISC
  *
  * This source code is licensed under the ISC license.
